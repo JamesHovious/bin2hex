f89b0b98526c6ebeb97cd3e1ab61308afbcce864
JamesHovious <hoviousj@gmail.com>
2018-09-15 23:17:54 +0200

    added multi line output

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # bin2hex
 
-A very simple utility to read an input file and output a hexfile in the format of `\x00`. 
+Bin2Hex is a command line tool that will parse a binary file and write out a a hex file in an escaped format (e.g. "\x90\41").
+
+```
+For example .\Bin2Hex.exe -i MyBytes.bin -o MyHex.hex
+
+Usage: 
+  -i, --InFile string    Path to the input file.
+  -o, --OutFile string   Path to the output file.
+  -l, --linebreak        Multiline output.
+  ```
--- bin2hex.go
+++ bin2hex.go
@@ -11,27 +11,35 @@ import (
 )
 
 var inFilePtr, outFilePtr *string
+var lineBreakPtr *bool
 var hexBytes []byte
 
 func insertNth(s string, n int) string {
 	var buffer bytes.Buffer
-	buffer.WriteRune('\\')
-	buffer.WriteRune('x')
 	var n1 = n - 1
 	var l1 = len(s) - 1
+	buffer.WriteRune('\\')
+	buffer.WriteRune('x')
 	for i, rune := range s {
-		buffer.WriteRune(rune)
+		if *lineBreakPtr && i%400 == 0 && i != 0 && i != l1 {
+			buffer.WriteRune('\r')
+			buffer.WriteRune('\n')
+		} else {
+			buffer.WriteRune(rune)
+		}
 		if i%n == n1 && i != l1 {
 			buffer.WriteRune('\\')
 			buffer.WriteRune('x')
 		}
+
 	}
 	return buffer.String()
 }
 
 func parseArgs() {
-	inFilePtr = flag.StringP("InFile", "i", "", " Path to the input file.")
-	outFilePtr = flag.StringP("OutFile", "o", "", " Path to the output file.")
+	inFilePtr = flag.StringP("InFile", "i", "", "Path to the input file.")
+	outFilePtr = flag.StringP("OutFile", "o", "", "Path to the output file.")
+	lineBreakPtr = flag.BoolP("linebreak", "l", false, "Multiline output.")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "\r\n"+
